b62ee6eae68d5f8d18eb8ae92ce880299e6b3ad9
hamzaplojovic <hamzaplojovic9@gmail.com>
2022-09-04 11:14:39 +0200

    removed props I am not using

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,13 @@ import { News } from "./Components/News/News";
 import { Search } from "./Components/News/Search";
 import { FullPage } from "./Components/News/FullPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
 
 export const App = () => {
-    const [pageNumber, setPageNumber] = useState(1);
-
     return (
         <NewsContextProvider>
             <BrowserRouter>
                 <Routes>
-                    <Route
-                        path="/"
-                        element={
-                            <News
-                                pageNumber={pageNumber}
-                                setPageNumber={setPageNumber}
-                            />
-                        }
-                    />
+                    <Route path="/" element={<News />} />
                     <Route path="/article" element={<FullPage />} />
                     <Route path="/news" element={<Search />} />
                 </Routes>
--- src/Components/News/News.tsx
+++ src/Components/News/News.tsx
@@ -1,15 +1,9 @@
 import { useContext } from "react";
 import { NewsContext } from "../../context/context";
 import { Mapping } from "./FilteringComponents/Mapping";
-import { Load } from "./FilteringComponents/LoadMore";
 import { Layout } from "../Layout/Layout";
 
-interface NewsInterface {
-    pageNumber: number;
-    setPageNumber: React.Dispatch<React.SetStateAction<number>>;
-}
-
-export const News = (props: NewsInterface) => {
+export const News = () => {
     const data: undefined = useContext(NewsContext);
     return (
         <Layout>
